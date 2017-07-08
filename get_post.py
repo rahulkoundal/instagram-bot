20bd78f417a01c15ccc1f84661c7fd2d74593eba
rahulshivu <rahulkoundal1996@gmail.com>
2017-07-08 17:35:05 +0530

    mynew file py file

---
+++ get_post.py
@@ -0,0 +1,18 @@
+from constants import *
+import requests
+def get_posts():
+    url=BASE_URL+'users/self/media/recent/?access_token=%s' %(APP_ACCESS_TOKEN)
+    print "GET request url %s " % url
+    request=requests.get(url).json()
+
+    if request['meta']['code']==200:
+        if len(request['data']):
+            print request['data'][0]['id']
+
+        else:
+            print "data not recieved"
+
+    else:
+        print "user does not exist"
+
+get_posts()
