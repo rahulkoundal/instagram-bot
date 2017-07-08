dacf43e2006ebdc93ad1127d4e6ef0f468b5ec53
rahulshivu <rahulkoundal1996@gmail.com>
2017-07-08 17:48:43 +0530

    update get_post.py

--- get_post.py
+++ get_post.py
@@ -2,11 +2,15 @@
 
 from constants import *
 import requests
+
+#get_posts function to get the user post
 def get_posts():
     url=BASE_URL+'users/self/media/recent/?access_token=%s' %(APP_ACCESS_TOKEN)
     print "GET request url %s " % url
     request=requests.get(url).json()
 
+#check the responce of the server
+    
     if request['meta']['code']==200:
         if len(request['data']):
             print request['data'][0]['id']
