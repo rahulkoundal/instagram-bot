8d57558bebc4dfaa6c8c3e848982ce046b6bf362
rahulshivu <rahulkoundal1996@gmail.com>
2017-07-08 17:18:33 +0530

    my 2nd commit in get_user_id.py file

--- get_user_id.py
+++ get_user_id.py
@@ -1,6 +1,7 @@
 import  requests
 from constants import BASE_URL ,APP_ACCESS_TOKEN
 
+#get_user_id function 
 def get_user_id(insta_username):
   request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
   print 'GET request url : %s' %(request_url)
