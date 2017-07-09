496144046a239b11545f7bb8b373b37aef82a19b
rahulshivu <rahulkoundal1996@gmail.com>
2017-07-09 23:04:53 +0530

    error free get_user_info.py

--- get_id.py
+++ get_id.py
@@ -1,16 +1,12 @@
-#get_user id _function
 import requests
 from constant import *
-from constant import APP_ACCESS_TOKEN,BASE_URL
 
-#get_user_id function
 
 def get_user_id(insta_username):
     request_url = (BASE_URL + 'users/search?q=%s&access_token=%s') % (insta_username, APP_ACCESS_TOKEN)
     print 'GET request url : %s' % (request_url)
     user_info = requests.get(request_url).json()
 
-#check wheather server responce or not
     if user_info['meta']['code'] == 200:
         if len(user_info['data']):
             return user_info['data'][0]['id']
