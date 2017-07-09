2860ec94db290f9f8736aeeae8a15458e68a0b8e
rahulshivu <rahulkoundal1996@gmail.com>
2017-07-09 10:13:08 +0530

    u        elif choice==b:

--- main.py
+++ main.py
@@ -1,4 +1,30 @@
 #main file
+import urllib3
 import requests
 from self_info import *
-self_info()
+from get_user_info import *
+
+def main():
+    while True:
+        print '\n'
+        print 'Hey! Welcome to instaBot!'
+        print 'Here are your menu options:'
+        print "a.Get your own details\n"
+        print "b.Get details of a user by username\n"
+
+        choice=raw_input("Enter you choice: ")
+        if choice=="a":
+            self_info()
+        elif choice=="b":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_user_id(insta_username)
+        elif choice=="c":
+            insta_username = raw_input("Enter the username of the user: ")
+            get_user_info(insta_username)
+
+        elif choice=="j":
+            exit()
+        else:
+            print "wrong choice"
+
+main()
