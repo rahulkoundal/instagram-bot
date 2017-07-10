9d15821e5772b21c838fb64d82bff0b1ef721de6
rahulshivu <rahulkoundal1996@gmail.com>
2017-07-10 08:02:09 +0530

    self_info.py have proper indentation

--- self_info.py
+++ self_info.py
@@ -1,3 +1,5 @@
+# import library and file
+
 import requests
 from constant import APP_ACCESS_TOKEN, BASE_URL
 import urllib
@@ -13,23 +15,23 @@ def self_info():
 #  to check wheather server is reply or not
 
     if user_info['meta']['code'] == 200:
-
-          if len(user_info['data']):
-            # username fetch
-            # print 'Username: %s' % (user_info['data']['username'])
-            username = user_info['data']['username']
-            print 'username:%s' % username
-            # no of followers,following,posts
-            print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
-            print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
-            print 'No. of posts: %s' % (user_info['data']['counts']['media'])
-            # fetch profile photo from instgram
-            image = user_info['data']['profile_picture']
-            image1 = user_info['data']['id'] + '.jpeg'
-            urllib.urlretrieve(image, image1)
-            print 'image is downloaded'
-          else:
-               print 'User does not exist!'
+            # len() function check whether server send the data
+            if len(user_info['data']):
+                # username fetch
+                # print 'Username: %s' % (user_info['data']['username'])
+                username = user_info['data']['username']
+                print 'username:%s' % username
+                # no of followers,following,posts
+                print 'No. of followers: %s' % (user_info['data']['counts']['followed_by'])
+                print 'No. of people you are following: %s' % (user_info['data']['counts']['follows'])
+                print 'No. of posts: %s' % (user_info['data']['counts']['media'])
+                # fetch profile photo from instgram
+                image = user_info['data']['profile_picture']
+                image1 = user_info['data']['id'] + '.jpeg'
+                urllib.urlretrieve(image, image1)
+                print 'image is downloaded'
+            else:
+                 print 'User does not exist!'
     else:
         print 'Status code other than 200 received!'
 
