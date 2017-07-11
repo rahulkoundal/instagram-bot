4f68a496b69cb6ed483637b7d94934a1ff79d9c5
rahulshivu <rahulkoundal1996@gmail.com>
2017-07-11 12:27:47 +0530

    hh

---
+++ objective.py
@@ -0,0 +1,32 @@
+#import all need labraries and functions from files
+from obj import *
+from get_id import *
+from new_obj import wordcloud
+
+#  function which fetch the hash tag comment from users post
+
+
+def get_hash_tag(insta_username):
+    user_id = get_user_id(insta_username)
+    if user_id == None:
+        print "user not exist"
+        exit()
+    url=BASE_URL+'users/%s/media/recent/?access_token=%s' %(user_id,APP_ACCESS_TOKEN)
+    print "GET requested url :%s" %url
+    req_media=requests.get(url).json()
+
+    # open a text file
+    file = open("name.txt",'w')
+    for posts in req_media['data']:
+
+    # write the caption text or hashtag coments in text file in utf-8 encoding
+        file.write(posts['caption']['text'].encode('utf-8'))
+    # close the file
+    file.close()
+
+
+    # call wordcloud  function for making word cloud of hash tag comment in a activity
+     wordcloud()
+
+    #call the fuction
+get_hash_tag(raw_input("enter"))
--- start.py
+++ start.py
@@ -4,30 +4,31 @@ from get_user_info import get_user_info
 from like_a_post import like_a_post
 from get_user_post import get_user_post
 from post_comment import post_comment
-# from delete_neg_comnt import del_neg_comment
 from get_own_post import get_own_post
+
 # from objective import get_hash_tag
 import time
-#def fuction for start instabot
+# def fuction for start instabot
+
+
 def init_bot():
-    #this list cotain users options
+    # this list cotain users options
     option_list = ['\t\t\t\t*************************Welcome to instabot*****************************',
                    'A.Get your own details', 'B..Get details of user by username', 'C..get your own recent post',
                    'D..get recent post of user by username ', 'E..Like most recent user post',
-                   'F..make comment on user recent post ', 'G..Delete negative comment from recent post',
-                   'H..Create a word cloud of user activity from users posts','...write exit to leave instabot']
+                   'F..make comment on user recent post ','...write exit to leave instabot']
 
     instabot = True
 
     while instabot:
-        #loop for print the option and time delay for each option is .200 microsecond
+        # loop for print the option and time delay for each option is .200 microsecond
         for x in range(0,len(option_list)):
             print(option_list[x],end='\n\n')
             time.sleep(.100)
 
         select_option = raw_input("Enter your choice::")
         try:
-            #Take user choice
+            # enter the choice
 
             select_option=select_option.upper()
             if select_option == 'A':
@@ -60,7 +61,7 @@ def init_bot():
             print ("This is not valid option")
         raw_input("Press enter to continue:\n\n\n ")
 
-#calling insta bot function
+# calling insta bot function
 init_bot()
 
 
